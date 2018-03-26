a3691df07fac4f774f34eb73ab3ae8a0ae6c3fb0
Levente Szabadkai <zeppike@gmail.com>
2018-03-26 09:59:42 +0200

    just for the funz
    Map and filter is discouraged, use a property instead of checking low level string match...

--- programmer/handlers/ceo_handler.py
+++ programmer/handlers/ceo_handler.py
@@ -8,8 +8,7 @@ class CEOHandler(Handler):
 
         if data['title'] == 'CEO':
             employee = CEO(**data)
-
-            employee.associates = map(lambda x: x.name, filter(lambda x: x.title.lower() == 'senior partner', company.employees))
+            employee.associates = [employee.name for employee in company.employees if employee.is_senior]
             employee.salary = company.base_salary * 5 * max([len(company.employees), 1])
 
             return employee
